592a611f00208cbecd23e9d71d139c7175c91d20
Theophilus <theophilusadesola002@gmail.com>
2026-03-21 10:42:35 +0100

    test: tested happy paths and edge cases for transfer and approve

--- contract/test/RBNT.t.sol
+++ contract/test/RBNT.t.sol
@@ -4,6 +4,21 @@ pragma solidity 0.8.33;
 import {Test} from "forge-std/Test.sol";
 import {RBNT} from "../src/RBNT.sol";
 
+event Transfer(address indexed _from, address indexed _to, uint256 _value);
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+// event TokensRequested(address indexed user, uint256 amount);
+// event TokensMinted(address indexed to, uint256 amount);
+// event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+
+// error Error__NotOwner();
+//     error Error__MaxSupplyExceeded();
+//     error Error__CooldownNotElapsed(uint256 nextAllowedTime);
+error Error__ZeroAddress();
+//     error Error__ZeroValue();
+error Error__InsufficientBalance();
+// error Error__InsufficientAllowance();
+
 contract RBNTTest is Test {
     RBNT public rbnt;
     uint256 constant INITIAL_SUPPLY = 200;
@@ -11,18 +26,27 @@ contract RBNTTest is Test {
     address recipient = makeAddr("recipient");
     address zeroAddress = address(0);
     uint256 transferAmount = 10;
+    uint256 approvedAmount = 100;
+    uint256 excessTransferAmount = 500;
+    address contractAddress;
 
     function setUp() public {
         rbnt = new RBNT(owner, INITIAL_SUPPLY);
+        contractAddress = address(rbnt);
     }
 
-    // ERC20Basic
+    /***  ERC20Basics
+     * transfer
+     * approve
+     */
 
     function test__successfulTransfer() public {
         uint256 prevOwnerBal = rbnt.balanceOf(owner);
         uint256 prevRecipientBal = rbnt.balanceOf(recipient);
         //transfer
         vm.prank(owner);
+        vm.expectEmit(true, true, false, false);
+        emit Transfer(owner, recipient, transferAmount);
         rbnt.transfer(recipient, transferAmount);
         uint256 currentOwnerBal = rbnt.balanceOf(owner);
         uint256 currentRecipientBal = rbnt.balanceOf(recipient);
@@ -30,6 +54,62 @@ contract RBNTTest is Test {
         assertEq(currentOwnerBal, prevOwnerBal - transferAmount);
         assertEq(currentRecipientBal, prevRecipientBal + transferAmount);
     }
+
+    function test__transferEdgeCases() public {
+        vm.startPrank(owner);
+
+        // Transfer more than balance → revert
+        vm.expectRevert(abi.encodeWithSelector(Error__InsufficientBalance.selector));
+        rbnt.transfer(recipient, excessTransferAmount);
+
+        // Transfer to address(0) → revert
+        vm.expectRevert(abi.encodeWithSelector(Error__ZeroAddress.selector));
+        rbnt.transfer(zeroAddress, transferAmount);
+
+        // Transfer full balance → works
+        uint256 prevOwnerBal = rbnt.balanceOf(owner);
+        uint256 prevRecipientBal = rbnt.balanceOf(recipient);
+
+        vm.expectEmit(true, true, false, false);
+        emit Transfer(owner, recipient, transferAmount);
+        rbnt.transfer(recipient, prevOwnerBal);
+
+        uint256 currentOwnerBal = rbnt.balanceOf(owner);
+        uint256 currentRecipientBal = rbnt.balanceOf(recipient);
+        assertEq(currentOwnerBal, 0);
+        assertEq(currentRecipientBal, prevRecipientBal + prevOwnerBal);
+
+        vm.stopPrank();
+    }
+
+    function test__successfulApproval() public {
+        vm.startPrank(owner);
+
+        //owner allows contract to spend funds
+        // Emits Approval event
+        vm.expectEmit(true, true, false, false);
+        emit Approval(owner, contractAddress, approvedAmount);
+        rbnt.approve(contractAddress, approvedAmount);
+
+        //check allowance
+        uint256 approvedAllowance = rbnt.allowance(owner, contractAddress);
+
+        // Approve sets allowance correctly (Overwriting allowance works)
+        assertEq(approvedAmount, approvedAllowance);
+
+        vm.stopPrank();
+    }
+
+    function test__ApprovalEdgeCases() public {
+        vm.startPrank(owner);
+
+        // Approve to zero address → revert
+        vm.expectRevert(abi.encodeWithSelector(Error__ZeroAddress.selector));
+        rbnt.approve(zeroAddress, approvedAmount);
+
+        vm.stopPrank();
+    }
+
     // Mint
     // Faucet
     // Ownership
